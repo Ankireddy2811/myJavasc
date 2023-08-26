cd0cb29671df36654875e9c8f5ced5f0356937e1
Ankireddy99 <20kb1a05d2@nbkrist.org>
2023-08-26 16:25:22 +0530

    mysecond commit

---
+++ abc.js
@@ -0,0 +1,5 @@
+const rootElement = document.getElementById("root");
+const element = document.createElement("h1");
+element.textContent = "Hello Anki!";
+element.classList.add("heading");
+rootElement.appendChild(element);
